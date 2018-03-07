279362a8e3879baa204a79d63cf48bae9904e33f
devandart <contact@devandart.de>
2018-03-07 15:54:18 +0100

    Compression Editor Field Values preparation

---
+++ ForgeNetworkingUnityEditor/ForgeCompressionSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ForgeCompressionSettings
+{
+    public bool compression = false;
+    public int min = 0;
+    public int max = 255;
+    public float accuracy = 0.01f;
+}
